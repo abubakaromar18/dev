e365df9af68c2e1ff9255da994e148ec16e470f1
abubakaromar18 <abubakaromar18@gmail.com>
2022-06-14 23:57:10 -0500

    06-14-22 update2

---
+++ web-projects/12-faq-cards/script.js
@@ -0,0 +1,9 @@
+const toggle = document.querySelectorAll('.faq-toggle')
+
+toggle.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.parentNode.classList.toggle('active')
+    })
+})
+
+
