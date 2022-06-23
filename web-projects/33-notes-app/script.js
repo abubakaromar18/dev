bacfa057214c7622ddc9819ee1eba2a2f926672e
abubakaromar18 <abubakaromar18@gmail.com>
2022-06-22 23:20:57 -0500

    6-22-22 update

---
+++ web-projects/33-notes-app/script.js
@@ -0,0 +1,46 @@
+const addBtn = document.getElementById('add')
+
+addBtn.addEventListener('click', () => addNewNote())
+
+function addNewNote(text = ''){
+    const note = document.createElement('div')
+    note.classList.add('note')
+
+    note.innerHTML = `
+    <div class="tools">
+        <button class="edit">
+            <div class="i fas fa-edit"></div>
+        </button>
+        <button class="delete">
+            <div class="i fas fa-trash-alt"></div>
+        </button>
+    </div>
+
+    <div class="main ${text ? "" : "hidden"}"></div>
+    <textarea class="${text ? "hidden" : ""}"></textarea>`
+
+    const editBtn = note.querySelector('.edit')
+    const deleteBtn = note.querySelector('.delete')
+    const main = note.querySelector('.main')
+    const textArea = note.querySelector('textarea')
+
+    textArea.value = text
+    main.innerHTML = marked.parse(text)
+
+    textArea.addEventListener('input', (e) => {
+       const {value } = e.target
+
+       main.innerHTML = marked.parse(value)
+    })
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden')
+        textArea.classList.toggle('hidden')
+    })
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove()
+    })
+
+    document.body.appendChild(note)
+}
